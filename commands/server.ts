e084b58b00c808b9baa8edb422cf5c0691cdba03
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-19 11:49:07 +0100

    feat: server command is now module

---
+++ commands/server.ts
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Provides information about the server."),
+  async execute(interaction) {
+    // interaction.guild is the object representing the Guild in which the command was run
+    try {
+      await interaction.reply(
+        `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+      );
+    } catch (err) {
+      console.error({ err, interaction, guild: interaction.guild });
+    }
+  },
+};
