4a7004530fcdd32097274278479238a311f1306c
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-19 12:21:39 +0100

    refactor: command set dry up

--- bot.ts
+++ bot.ts
@@ -15,14 +15,15 @@ const commandFiles = fs
 commandFiles.forEach((file) => {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    coterie.commands.set(command.data.name, command);
-  } else {
-    console.log(
+  const couldSetCommand = !!command?.data?.name && !!command?.execute;
+
+  if (!couldSetCommand) {
+    return console.log(
       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
     );
   }
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  coterie.commands.set(command.data.name, command);
 });
 
 coterie.once(Events.ClientReady, (c) => {
