e4294325844fc85fff774473fe92c67e4426e900
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-19 12:25:13 +0100

    refactor: coterie on interaction internal control flow improved

--- bot.ts
+++ bot.ts
@@ -34,17 +34,18 @@ coterie.login(process.env.DISCORD_TOKEN);
 
 coterie.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction?.client?.commands?.get(interaction.commandName);
 
   if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+    return console.error(
+      `No command matching ${interaction.commandName} was found.`
+    );
   }
 
   try {
     await command.execute(interaction);
   } catch (error) {
-    console.error(error);
+    console.error({ error });
     await interaction.reply({
       content: "There was an error while executing this command!",
       ephemeral: true,
