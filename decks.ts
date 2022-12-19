12a9826920523484b2050ca6c3666d5d9db8d1b0
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-19 21:22:33 +0100

    feat: bot has now decks archive mock

---
+++ decks.ts
@@ -0,0 +1,173 @@
+type Archetype = "wall" | "politics" | "combat" | "ally" | "bleed" | "combo";
+
+type Deck = {
+  id: string;
+  vdbURL: string;
+  name: string;
+  archetypes: Archetype[];
+};
+
+export const decks: Deck[] = [
+  {
+    id: "foyy4obvlh",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "qlsbefzq4fs",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "y03kj0ia9ke",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "lsya5lu5qy",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "uzk4zjthihd",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "2lz4u1ag3h3",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "v0fgxj0dtce",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "ygaotmybs0i",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "r6smbbqmb2d",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "hc42jmvfgzq",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "0vaihjsic51",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "qwydwfi211e",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "yriu1qlkp1",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "2fxq7jeuker",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "lt8zipipw4q",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "5ksyno5xgjj",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "ykv1wp3wy2",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "5y5jffezaqq",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "wzkocp5dsx9",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "0694w52evs0a",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "s9eqej2bnnj",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "ynhpqsoszs",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "b2hukw0rnjb",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "gtexpph3us7",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "6immdq65fv8",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "luwslohx8er",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+  {
+    id: "dicih35r0q",
+    vdbURL: "",
+    name: "",
+    archetypes: [],
+  },
+];
