7ced84140a9783f70f1e5fd40ca6bbb66fd64674
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-19 11:59:50 +0100

    WIP: bot refactor with discord slash API

--- bot.ts
+++ bot.ts
@@ -1,24 +1,52 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
-const token = process.env.DISCORD_TOKEN;
+const coterie = new Client({ intents: [GatewayIntentBits.Guilds] });
+coterie.commands = new Collection();
 
-const coterieClient = new Client({ intents: [GatewayIntentBits.Guilds] });
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-coterieClient.once(Events.ClientReady, (c) => {
+commandFiles.forEach((file) => {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    coterie.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+});
+
+coterie.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-coterieClient.login(token);
+coterie.login(process.env.DISCORD_TOKEN);
 
-const handleMessage = (message) => {
-  console.log({ message });
-};
+coterie.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-coterieClient.on("message", (message) => handleMessage(message));
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
