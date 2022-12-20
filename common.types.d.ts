aa116bad20ff8b2b58bc20e48561ffbd0317af34
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-20 09:31:22 +0100

    feat: types are now in common.type

---
+++ common.types.d.ts
@@ -0,0 +1,32 @@
+export type Id = string;
+export type Order = 1 | 2 | 3 | 4 | 5;
+
+export type Player = {
+  id: Id;
+  name: string;
+  seating: Order;
+  archetype: Archetype;
+  order: Order;
+};
+
+export type Archetype =
+  | "wall"
+  | "politics"
+  | "combat"
+  | "ally"
+  | "bleed"
+  | "combo";
+
+export type Deck = {
+  id: string;
+  vdbURL: string;
+  name: string;
+  archetypes: Archetype[];
+};
+
+export type CoterieCommand = {
+  name: String;
+  action: (args?: Record<string, any>) => void;
+};
+
+export type CommonObj = Record<string, any>;
