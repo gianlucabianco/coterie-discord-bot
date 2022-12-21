6fe9dd2b799add4ae40d5e2927d772a373490ffe
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-21 10:15:58 +0100

    chore: commands.config is now linted

--- commands/commands.config.ts
+++ commands/commands.config.ts
@@ -1,4 +1,4 @@
-export const prefix = "!";
-export const separator = " ";
-export const argsIdentifier = ":";
-export const argsSeparator = "-";
+export const prefix = "!"
+export const separator = " "
+export const argsIdentifier = ":"
+export const argsSeparator = "-"
