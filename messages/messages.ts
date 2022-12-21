a393af6336102edecea343c08188ce9081598383
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-21 10:09:14 +0100

    chore: messages is now linted

--- messages/messages.ts
+++ messages/messages.ts
@@ -1,53 +1,48 @@
-import { separator, prefix } from "./../commands/commands.config";
-import { commands, handleCommands } from "../commands";
-import type { CommonObj } from "../common.types";
+import { separator, prefix } from "./../commands/commands.config"
+import { commands, handleCommands } from "../commands"
+import type { CommonObj } from "../common.types"
 
 const getRawMessages = (message: CommonObj): string[] => {
-  const prefixPurgedMessage = message.content.slice(1);
-  return prefixPurgedMessage.toLowerCase().split(separator);
-};
+	const prefixPurgedMessage = message.content.slice(1)
+	return prefixPurgedMessage.toLowerCase().split(separator)
+}
 
-const getUniqueMessages = (messages: string[]): string[] => [
-  ...new Set(messages),
-];
+const getUniqueMessages = (messages: string[]): string[] => [...new Set(messages)]
 
 const getValidAndInvalidCommands = (messages: string[]) =>
-  messages.reduce(
-    (accumulator, currMessage) => {
-      const command = commands.find(
-        (cmd: CommonObj) => cmd.name === currMessage
-      );
+	messages.reduce(
+		(accumulator, currMessage) => {
+			const command = commands.find((cmd: CommonObj) => cmd.name === currMessage)
 
-      if (!command) {
-        currMessage && accumulator.invalidCommands.push(currMessage);
-        return accumulator;
-      }
+			if (!command) {
+				currMessage && accumulator.invalidCommands.push(currMessage)
+				return accumulator
+			}
 
-      accumulator.validCommands.push(command);
+			accumulator.validCommands.push(command)
 
-      return accumulator;
-    },
-    {
-      validCommands: [] as CommonObj[],
-      invalidCommands: [] as string[],
-    }
-  );
+			return accumulator
+		},
+		{
+			validCommands: [] as CommonObj[],
+			invalidCommands: [] as string[],
+		}
+	)
 
 export const handleMessage = (message: CommonObj) => {
-  if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(prefix)) return
 
-  const rawMessages = getRawMessages(message);
+	const rawMessages = getRawMessages(message)
 
-  const uniqueMessages = getUniqueMessages(rawMessages);
+	const uniqueMessages = getUniqueMessages(rawMessages)
 
-  const { validCommands, invalidCommands } =
-    getValidAndInvalidCommands(uniqueMessages);
+	const { validCommands, invalidCommands } = getValidAndInvalidCommands(uniqueMessages)
 
-  if (invalidCommands.length) {
-    return message.channel.send(
-      `These are not valid Coterie commands: ${invalidCommands}. Please remove invalid commands and type valid commands.`
-    );
-  }
+	if (invalidCommands.length) {
+		return message.channel.send(
+			`These are not valid Coterie commands: ${invalidCommands}. Please remove invalid commands and type valid commands.`
+		)
+	}
 
-  handleCommands(validCommands);
-};
+	handleCommands(validCommands)
+}
