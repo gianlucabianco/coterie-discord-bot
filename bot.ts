a010ae62b3869982234a5efd75d0ad192eaf347b
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-21 10:09:37 +0100

    chore: bot is now linted

--- bot.ts
+++ bot.ts
@@ -1,23 +1,20 @@
-import { handleMessage } from "./messages/messages";
+import type { CommonObj } from "./common.types"
+import { handleMessage } from "./messages/messages"
 
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits } = require("discord.js")
 
-require("dotenv").config();
+require("dotenv").config()
 
 const coterie = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+})
 
-coterie.once(Events.ClientReady, (client: Record<string, any>) => {
-  console.log(`Coterie is online! Logged in as ${client?.user?.tag}`);
-});
+coterie.once(Events.ClientReady, (client: CommonObj) => {
+	console.log(`Coterie is online! Logged in as ${client?.user?.tag}`)
+	const list = client?.guilds?.cache?.get(process.env.SERVER_ID)
+	list.members.cache.forEach((member: any) => console.log(member.user.username))
+})
 
-coterie.on("messageCreate", (message: Record<string, any>) =>
-  handleMessage(message)
-);
+coterie.on("messageCreate", (message: CommonObj) => handleMessage(message))
 
-coterie.login(process.env.DISCORD_TOKEN);
+coterie.login(process.env.DISCORD_TOKEN)
