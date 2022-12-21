889ad98040af5d0ed31e51fb3fa3b78d3c6cac56
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-21 10:15:28 +0100

    feat: bot has now prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 120, // Newline string threshold
+	tabWidth: 4, // Set the number of spaces for each horizontal indentation of the tool
+	useTabs: true,
+	semi: false, // Removes semicolons
+	vueIndentScriptAndStyle: true,
+	bracketSpacing: true,
+	arrowParens: "avoid", // Omit parens when possible. Example: x => x
+	insertPragma: false, // There is no need to automatically add... At the beginning of the file @prettier
+	endOfLine: "lf", // Line breaks use lf
+}
