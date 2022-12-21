230419a3b1b8c969668ffe7542cdd4974b810bc1
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-21 10:08:32 +0100

    chore: commands is now linted

--- commands/index.ts
+++ commands/index.ts
@@ -1,47 +1,63 @@
-import type { CommonObj, CoterieCommand } from "../common.types";
+import type { CommonObj, CoterieCommand } from "../common.types"
 
 const drive = () => {
-  console.log({ action: "drive!" });
-};
+	console.log({ action: "drive!" })
+}
 
 const forcePlayers = (args: CommonObj) => {
-  console.log({ ...args });
-};
+	console.log({ ...args })
+}
 
 const forceDecks = (args: CommonObj) => {
-  console.log({ ...args });
-};
+	console.log({ ...args })
+}
 
 const registerGame = (args: CommonObj) => {
-  console.log({ ...args });
-};
+	console.log({ ...args })
+}
 
 export const commands: CoterieCommand[] = [
-  {
-    name: "drive",
-    action: () => drive(),
-  },
-  {
-    name: "fp",
-    action: (args) => args && forcePlayers(args),
-  },
-  {
-    name: "fd",
-    action: (args) => args && forceDecks(args),
-  },
-  {
-    name: "register",
-    action: (args) => args && registerGame(args), // TODO: needs the game id, should be invoked after the main drive command
-  },
-];
+	{
+		name: "drive",
+		action: () => drive(),
+	},
+	{
+		name: "player", // FORCE PLAYER(S) // TODO: chose meaningful/useful names // TODO: commands could come from parsing and should be used as args and could be chainable
+		action: (args) => args && forcePlayers(args),
+	},
+	{
+		name: "deck", // FORCE DECK(S)
+		action: (args) => args && forceDecks(args),
+	},
+	{
+		name: "archetype", // FORCE ARCHETYPE(S)
+		action: (args) => args && forceDecks(args),
+	},
+	{
+		name: "seating", // FORCE SEATING(S)
+		action: (args) => args && forceDecks(args),
+	},
+	{
+		name: "predator", // FORCE PREDATOR
+		action: (args) => args && forceDecks(args),
+	},
+	{
+		name: "prey", // FORCE PREY
+		action: (args) => args && forceDecks(args),
+	},
+	{
+		name: "register",
+		action: (args) => args && registerGame(args), // TODO: needs the game id, should be invoked after the main drive command
+	},
+]
 
 export const handleCommands = (commands: CommonObj[]) => {
-  const drive = commands.find((command) => command.name === "drive");
-  const secondaryCommands = commands.filter(
-    (command) => command.name !== "drive"
-  );
+	const drive = commands.find((command) => command.name === "drive")
+	const secondaryCommands = commands.filter((command) => command.name !== "drive")
 
-  secondaryCommands.forEach((command) => command.action());
+	console.log({ secondaryCommands })
 
-  drive?.action();
-};
+	secondaryCommands.forEach((command) => command.action())
+
+	drive?.action()
+}
