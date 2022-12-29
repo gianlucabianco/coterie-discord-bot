64c55ee6f12ec557f30d7cb51d388700c6f2a804
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-12-29 17:12:55 +0100

    feat: commands.config stores now startingCommands

--- commands/commands.config.ts
+++ commands/commands.config.ts
@@ -3,3 +3,14 @@ export const prefix = "!"
 export const separator = " "
 export const argsIdentifier = ":"
 export const argsSeparator = "-"
+
+export const startingCommands = ["!help", "!showdecks", "!drive"]
+
+/*
+QUERY EXAMPLE:
+!drive: miro(arch-wall prey-3 predator-ale) gas(arch-ccci prey-1) ale chiara
+!drive => main command
+: => main command / args separator
+() => wraps args subcommands
+sub sub commands => command-args
+*/
