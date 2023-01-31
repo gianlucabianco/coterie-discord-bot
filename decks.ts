56f837664015a4f233805ae75152a333c29c6e6f
Gianluca Bianco <ciao@gianlucabianco.dev>
2023-01-31 10:20:06 +0100

    chore: TODO removed

--- decks.ts
+++ decks.ts
@@ -1,12 +1,5 @@
 import type { Deck } from "./common.types"
-/*
-TODO: add missing decks => 
-- unnamed classic
-- kit aus
-- weenie dem
-- laibon picchio di otso
-- gangrel wall mio
-*/
+
 export const decks: Deck[] = [
 	{
 		id: "b2hukw0rnjb",
